0a841dd0e7a8f527a161c579bb3eeb6d9cccdb4c
scratchglory <scratchglory@gmail.com>
2022-05-15 20:16:31 -0400

    added details to the app, changd CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Footer from './containers/footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="profile-image">
+        <img src="https://imgs.search.brave.com/lljs8z0-G5NkKn_mzQAiru0dKjTRqN_NE9qxBYXEi18/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5S/ZmZGUlNnak9SX0ZV/azJ4SWF4R1BBSGFF/OCZwaWQ9QXBp" alt="head shot of hamburgler"/>
+      </div> {/* emd or "profile-image" */}
+
+        <div className="profile-header">
+          <h2>Laura Smith</h2>
+          <h4>Frontend Developer</h4>
+          <a href="www.google.com">laurasmith.website</a>
+        </div> {/* end of "profile-information" */}
+        
+        <div className="contact-info">
+          <button type="button" onclick="alert('hello world!')">Email ME!</button>
+          <button type="button" onclick="alert('hello world!')">LinkedIn</button>
+        </div>
+
+
+        <div className="profile-details">
+          <div className="About">
+            <h3>About</h3>
+            <p>lorem ipsum dolor sit amet, consectetur adipiscing</p>
+          </div>
+          <div className="Interests">
+            <h3>Interests</h3>
+            <p>lorem ipsum dolor sit amet, consectetur adipiscing</p>
+          </div>
+        </div> {/* end of "profile-details" */}
+
+      <Footer />
+    </div> // end of App 
   );
 }
 
